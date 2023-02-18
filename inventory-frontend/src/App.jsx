5ea581668931ebd741bf81fc189b3283a1882ec7
maminou <maminou.ayadi@icloud.com>
2023-02-19 00:12:06 +0100

    init

---
+++ inventory-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import { Orders } from './Components/Orders';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Products } from './Components/Products';
+import { ProductsCreate } from './Components/ProductsCreate';
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Products />} />
+      <Route path="/create" element={<ProductsCreate />} />
+      <Route path="/buy" element={<Orders/>} />
+    </Routes>
+  );
+}
+
+export default App;
